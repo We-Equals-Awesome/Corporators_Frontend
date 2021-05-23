e7f1e336b243481f32af1e83a622c7e3dedc3598
Srikanth-Kandarp <srikanthkandarp23@gmail.com>
2021-05-23 21:13:36 +0530

    Updated Stuff

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 //import 'package:cropapp/UI/citizenPage.dart';
+import 'package:cropapp/Screens/Complaints/complaint_page_2.dart';
 import 'package:flutter/material.dart';
-import 'Screens/login_and_signup/login_page1.dart';
+import 'package:geocoder/geocoder.dart';
+
+import 'Screens/Admin/wall_feed_new.dart';
+import 'Screens/Admin/wall_feed_updated.dart';
+
 //import 'Screens/Navigation_Bar.dart';
 
 void main() {
@@ -12,14 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: true,
-      /*
-       *  Value 0 is passed to MyhomePage.
-       *  This value is used to decide whether or not to show the ALterDialogue box in login page.
-       *  When the user opens the app then the AlterDialogue is not shown
-       *  When the user is redirected to the Login page from the 'EnterYourDetailsPage',
-       *   then the AlterDialogue Box is  shown.
-      */
-      home: LoginPage1(0), //LoginPage1(0),
+      home: WallFeedUpdated(), //LoginPage1(0),
     );
   }
 }
