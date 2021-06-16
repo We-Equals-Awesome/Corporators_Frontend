ad40d1d2a8a8ce0979361be42edf37351bec4dd3
dhiraj2803 <dheerajgupta2800@gmail.com>
2021-06-16 16:31:57 +0530

    min SDK changed to 21

--- lib/Screens/Admin/Admin_Complaints_1.dart
+++ lib/Screens/Admin/Admin_Complaints_1.dart
@@ -1,7 +1,6 @@
 import 'package:Corporator_Mobile_App/Dummy_Data/Admin-complaint.dart';
 import 'package:flutter/material.dart';
 import 'package:Corporator_Mobile_App/Utils/Colors.dart';
-
 import 'Admin_Complaints_2.dart';
 
 class AdminComplaintview extends StatelessWidget {
