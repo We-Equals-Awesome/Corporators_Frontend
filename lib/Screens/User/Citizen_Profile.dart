e30a3142b5a8f3aece66bf740c22e229b58a3e83
Apoorva10100 <47321161+Apoorva10100@users.noreply.github.com>
2021-06-17 20:45:17 +0530

    added change image functionality to citizen details

--- lib/Screens/User/Citizen_Profile.dart
+++ lib/Screens/User/Citizen_Profile.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:Corporator_Mobile_App/Utils/Colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:image_picker/image_picker.dart';
 
 class CitizenPage extends StatefulWidget {
   @override
@@ -15,52 +19,95 @@ List details = [
 ];
 
 class _CitizenPageState extends State<CitizenPage> {
+  PickedFile _imageFile; //this variable stores the selected profile image
+  final ImagePicker _picker = ImagePicker();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: background,
       body: SafeArea(
-        child: Center(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 30.0),
-                child: CircleAvatar(
-                  backgroundColor: textBoxBack,
-                  radius: 50,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  'This is the name',
-                  style: TextStyle(
-                    fontSize: 28,
-                    color: text,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'ProductSans',
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                imageProfile(),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'This is the name',
+                    style: TextStyle(
+                      fontSize: 28,
+                      color: text,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'ProductSans',
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.03,
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  _textForm("Phone Number", details[0], context),
-                  _textForm("Email", details[1], context),
-                  _textForm("Date of Birth", details[2], context),
-                  _textForm("Voter ID", details[3], context),
-                  _textForm("Aadhar Number", details[4], context)
-                ],
-              ),
-            ],
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.03,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    _textForm("Phone Number", details[0], context),
+                    _textForm("Email", details[1], context),
+                    _textForm("Date of Birth", details[2], context),
+                    _textForm("Voter ID", details[3], context),
+                    _textForm("Aadhar Number", details[4], context)
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
+  }
+
+  Widget imageProfile() {
+    return Center(
+        child: Container(
+      width: MediaQuery.of(context).size.width,
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(top: 20, bottom: 8),
+            child: CircleAvatar(
+              backgroundColor: background,
+              backgroundImage: _imageFile == null
+                  ? AssetImage('assets/icons/user.png')
+                  : FileImage(File(_imageFile.path)),
+              radius: MediaQuery.of(context).size.width * 0.12,
+            ),
+          ),
+          Positioned(
+              top: MediaQuery.of(context).size.height * 0.09,
+              left: MediaQuery.of(context).size.width * 0.53,
+              child: RawMaterialButton(
+                constraints: BoxConstraints.tight(Size(
+                    MediaQuery.of(context).size.width * 0.07,
+                    MediaQuery.of(context).size.width * 0.07)),
+                onPressed: () {
+                  takephoto(ImageSource.gallery);
+                },
+                elevation: 1.0,
+                fillColor: textBoxBack,
+                child: SvgPicture.asset("assets/icons/camera.svg"),
+                padding: EdgeInsets.all(1.0),
+                shape: CircleBorder(),
+              )),
+        ],
+      ),
+    ));
+  }
+
+  void takephoto(ImageSource source) async {
+    final pickedFile = await _picker.getImage(source: source);
+    setState(() {
+      _imageFile = pickedFile;
+    });
   }
 
   Widget _textForm(String a, String b, BuildContext context) {
